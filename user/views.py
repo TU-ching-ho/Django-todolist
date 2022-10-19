d7aa2582ffd54da1e434c344f14f86e55b14d0a2
mark <gh850702@gmail.com>
2022-10-19 16:46:24 +0800

    完成註冊登入頁面

--- user/views.py
+++ user/views.py
@@ -1,7 +1,43 @@
 from email import message
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+def user_logout(request):
+    logout(request)
+    return redirect('profile')
+def profile(request):
+    return render(request, './user/profile.html')
+
+
+def user_login(request):
+    username, message = '', ''
+    if request.method == 'POST':
+        if request.POST.get('login'):  # 按下登入按鈕
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            # 檢查帳號密碼是否為空
+            if username == '' or password == '':
+                message = '帳號跟密碼不能為空'
+            else:
+                # 檢查資料庫是否有該使用者
+                # 匹配密碼進行登入
+                user = authenticate(
+                    request, username=username, password=password)
+                if user is None:
+                    if User.objects.filter(username=username):
+                        message = '密碼有誤'
+                    else:
+                        message = '帳號有誤'
+                else:
+                    login(request, user)
+                    message = '登入中'
+                    return redirect('profile')
+        elif request.POST.get('register'):
+            return redirect('register')
+    return render(request, './user/login.html', {'message': message, 'username': username})
+
 
 # Create your views here.
 
